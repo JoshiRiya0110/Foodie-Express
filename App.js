30dd5dcc53921719533f4b9310334ea3304575f8
joshiriya0110 <joshiriya0110@gmail.com>
2024-02-24 05:06:48 +0530

    installed npm and parcel

--- App.js
+++ App.js
@@ -1,18 +1,9 @@
-/*
-    <div id="parent">
-        <div id="child">
-            <h1></h1>
-            <h2></h2>
-        </div>
-    <div/>
-
-    siblings tag
-    ReactElement is an object => after rendering it becomes tag
-*/
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, React.createElement("div", {
     id: "child"
-}, [React.createElement("h1", {}, "I am an h1 tag"), React.createElement("h2", {}, "I am an h2 tag")]));
+}, [React.createElement("h1", {}, "This is foodie express"), React.createElement("h2", {}, "I am an h2 tag")]));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent); //it is converting the heading into h1 tag
