6f9127919e74091ea9855307b842555874e1f027
joshiriya0110 <joshiriya0110@gmail.com>
2024-02-23 11:31:58 +0530

    creating nested sibling structures of html using react

--- App.js
+++ App.js
@@ -2,14 +2,16 @@
     <div id="parent">
         <div id="child">
             <h1></h1>
+            <h2></h2>
         </div>
     <div/>
-    
+
+    siblings tag
     ReactElement is an object => after rendering it becomes tag
 */ 
 
 const parent = React.createElement("div",{id:"parent"}, React.createElement("div", {
-    id:"child"},React.createElement("h1",{},"I am an h1 tag")));
+    id:"child"},[React.createElement("h1",{},"I am an h1 tag"), React.createElement("h2",{},"I am an h2 tag")]));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent); //it is converting the heading into h1 tag
