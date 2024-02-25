1d092480612e3cac5fcbd2f5029be7eeb89c165a
joshiriya0110 <joshiriya0110@gmail.com>
2024-02-25 15:18:25 +0530

    creating header, restaurant card, and body component inside App.js

--- App.js
+++ App.js
@@ -1,33 +1,59 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React Element
-const jsxHeading = (
-  <h1 id="heading" className="head">
-    Hello World
-  </h1>
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={require("./logo.png")} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>About us</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-//jsx takes care of injection attacks, it sanitizes our data
-//React Component
-const Title = () => (
-  <div>
-    <h1>{jsxHeading}</h1>
-    <h2>React element inside react component</h2>
-  </div>
+const RestaurantCard = () => {
+  return(
+    <div className="res-card">
+      <img className="res-logo" alt="res-logo" src={require("./logo.png")}/>
+      <h3>Meghana foods</h3>
+      <h4>Biryani, North Indian, Asian</h4>
+      <h4>4.4 stars</h4>
+      <h4>30 min delivery</h4>
+    </div>
+  )
+}
 
-)
-
-//component composition => nested components
-const Func = () => (
-  <div id="container">
-    {Title()}
-    <Title></Title>
-    <Title/>
-    {1000}
-    <h1 className="head">Hi this is Riya</h1>
-  </div>
-);
+const Body = () => {
+  return(
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+      </div>
+    </div>
+  )
+}
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Func/>);
+root.render(<AppLayout />);
